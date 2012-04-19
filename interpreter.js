48f65d3fd0b8db8de2274148189da508a9c2f152
Kaique Silva <kaiquesilva@visie.com.br>
2012-04-19 01:01:29 -0300

    in progress

--- interpreter.js
+++ interpreter.js
@@ -12,14 +12,14 @@ var Brainfuck = (function () {
         var limit = 300*100;
         
         while ( c < limit ) {
-            output.push(0);
+            output.push('0');
             c += 1;
         }
 
         return output;
     } ());
 
-    _.prototype.memory = (function ( buffer ) {
+    var __memory = (function ( buffer ) {
         var output = false;
         buffer = buffer || '';
 
@@ -39,8 +39,6 @@ var Brainfuck = (function () {
         '-', 
         '.', 
         ',',
-        '[',
-        ']',
         '#',
     ];
     // CommandsExecution
@@ -48,62 +46,71 @@ var Brainfuck = (function () {
     
     // Increment the pointer
     commandsExecution.incrementPointer = function () {
-        ++_pointer;
+        _pointer += 1;
 
         return true;
     };
     // Decrement the pointer
     commandsExecution.decremenetPointer = function () {
-        --_pointer;
+        if ( _pointer >= 0 ) {
+            _pointer -= 1;
+        }
 
         return true;
     };
     // Increment the byte in pointer
-    commandsExecution.byteIncrement = function ( pointerValue ) {
+    commandsExecution.byteIncrement = function ( inLoop ) {
         var output = false;
-        pointerValue = pointerValue || '';
+        var pointerValue = __memory[ _pointer ];
 
-        if ( pointerValue ) {
+        if ( typeof pointerValue === 'number' || pointerValue ) {
             // Casting to number
-            output =  pointerValue.toString();
-            output += _pointer;
+            output =  pointerValue;
+            output += '1';
+            __memory[ _pointer ] = output;
+            output = true;
         }
 
         return output;
     };
     // Decrement the byte in pointer
-    commandsExecution.byteDecrement = function ( pointerValue ) {
+    commandsExecution.byteDecrement = function () {
         var output = false;
-        pointerValue = pointerValue || '';
+        var pointerValue = __memory[ _pointer ];
 
         if ( pointerValue ) {
             // Casting to number
             output = pointerValue.toString();
-            output -= _pointer;
+            output -= '1';
+            __memory[ _pointer ] = output;
+            output = true;
         }
 
         return output;
     };
     // Return the byte at the pointer
-    commandsExecution.byteOutput = function ( pointerValue ) {
+    commandsExecution.byteOutput = function () {
         var output = false;
-        pointerValue = pointerValue || '';
+        var pointerValue = __memory[ _pointer ];
 
-        if ( pointerValue ) {
-            output = String.fromCharCode( pointerValue );    
+        if ( typeof pointerValue === 'number' || typeof pointerValue === 'string'  || pointerValue ) {
+            output = String.fromCharCode( pointerValue );
         }
 
         return output;
     };
     // Input a byte and store it in the byte at the pointer.
-    commandsExecution.byteInput = function ( pointerValue, input ) {
+    commandsExecution.byteInput = function ( input ) {
         var output = false;
-        pointerValue = pointerValue || '';
+        var pointerValue = __memory[ _pointer ];
+        input = input || '';
 
-        if ( pointerValue ) {
+        if ( input ) {
             output = pointerValue.toString();
             output += input.toString();
             output += _pointer;
+            __memory[ _pointer ] = output;
+            output = true;
         }
 
         return output;
@@ -111,9 +118,9 @@ var Brainfuck = (function () {
 
     var _commands = {};
     // > assign to function
-    _commands[ commands[0] ] = commandsExecution.incrementPointer();
+    _commands[ commands[0] ] = commandsExecution.incrementPointer;
     // < assign to function
-    _commands[ commands[1] ] = commandsExecution.decremenetPointer();
+    _commands[ commands[1] ] = commandsExecution.decremenetPointer;
     // + assign to function
     _commands[ commands[2] ] = commandsExecution.byteIncrement;
     // - assign to function
@@ -123,25 +130,49 @@ var Brainfuck = (function () {
     // , assign to function
     _commands[ commands[5] ] = commandsExecution.byteInput;
 
+    _.prototype.commands = _commands;
+    _.prototype.memory = __memory;
+
+    // Verify if exist a item in the array
+    var hasInDeepList = function ( target, arrayList ) {
+        var output = false;
+        target = target || '';
+        arrayList = arrayList || '';
+
+        if ( typeof target === 'number' || target && arrayList ) {
+            var l = arrayList;
+            for ( var i in l ) {
+                if ( target === l[i] ) {
+                    output = true;
+                }
+            }
+        }
+
+        return output;
+    };
+
     _.prototype.run = function ( input ) {
         var output = '';
-        var self = this;
+        var counterDeep = 0;
+        var lastDeep = 0;
         
-        console.log('Input:')
         if ( input.length > 0 ) {
-            console.log('For:')
             for ( var i = 0; i < input.length; i += 1 ) {
-                var isFunction = typeof _commands[ input[i] ] === 'function';
-                
-                if ( input[i] in commands ) {
-                    switch( input[i] ) {
-                        case commands[0]: _commands[ input[ i ] ]; break;   
-                        case commands[1]: _commands[ input[ i ] ]; break;
-                        case commands[2]: _commands[ input[ i ] ]( self.memory[ _pointer ] ); break;
-                        case commands[3]: _commands[ input[ i ] ]( self.memory[ _pointer ] ); break;
-                        case commands[4]: 
-                            output += _commands[ input[ i ] ]( self.memory[ _pointer ] );
-                        case commands[5]: _commands[ input[ i ] ]( self.memory[ _pointer ], '' ); break;
+                if ( hasInDeepList( input[i], commands ) ) {
+                    if ( input[i] === commands[4] ) {
+                        output += _commands[ input[i] ]();
+                    } else {
+                        _commands[ input[i] ]();
+                    }
+                } if ( input[i] === '[' ) {
+                    counterDeep += 1;
+                } if ( input[i] === ']' ) {
+                    counterDeep -= 1;
+                } if ( counterDeep > 0 && counterDeep > lastDeep ) {
+                    lastDeep = counterDeep;
+
+                    if ( input[i] === '>' || input[i] === '<' ) {
+                        _commands[ input[i] ]();    
                     }
                 }
             }    
